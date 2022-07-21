c05f0ac614432ca53e03bfe038e7689fc2691683
CatWalk2159 <97532246+CatWalk2159@users.noreply.github.com>
2022-07-21 23:48:17 +0900

    Add files via upload

---
+++ susiki/main/js/main.js
@@ -0,0 +1,14 @@
+function btm1(){
+    location.href ="/Users/kasaikoutarou/Desktop/susiki/syo1/index.html"
+}
+function btm2(){
+    location.href ="/Users/kasaikoutarou/Desktop/susiki/tyu1/index.html"
+}
+
+document.getElementById("let").addEventListener("mouseover", function(){
+	document.getElementById("mous").style.display = 'block';
+}, false);
+
+document.getElementById("let").addEventListener("mouseout", function(){
+	document.getElementById("mous").style.display = 'none';
+}, false);
